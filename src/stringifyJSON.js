857f0861893754eb24302eced2322640ff69107b
NickRashad <nrsternes@gmail.com>
2019-01-12 07:09:07 -0600

    Add strategy && git remotes

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //Strategy: take in element and return a string of the element's values in their original formats. Recurse over element and check type of each item. If it is a stringifiable array or object then we recurse. Otherwise
+  //create resultArr
+  //begin iteration
+  //if not array obj
+    //push item into resultArr
+  //else
+    //
+  //end iteration
+
+  //return resultArr
 };
+
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+];
